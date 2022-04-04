a24b8d94fe05e3cb28570af9ce933fe5ab48d0a4
Anton Golubenko <77355470+d0nate110@users.noreply.github.com>
2022-04-04 21:33:50 +0200

    Updated the "if" statement
    The car can now avoid obstacles that are at most 50 cm away from it.

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -34,7 +34,7 @@ void loop() {
 
     Serial.println(frontSensor.getDistance());
     delay(100);
-    if(frontSensor.getDistance() >= 50){
+    if(frontSensor.getDistance() <= 50 && frontSensor.getDistance() != 0){
         stopCar();
     }
     else {
