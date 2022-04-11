dc0b99349046a9c70ed425b4a03d5e721039649a
Akuien <91293028+Akuien@users.noreply.github.com>
2022-04-11 18:16:22 +0200

    User interface designs for the login screen, create account screen and health issues categories screen.

---
+++ CarEshipApp/app/src/main/java/com/example/careshipapp/MainActivityCategoryList.java
@@ -0,0 +1,14 @@
+package com.example.careshipapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityCategoryList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_category_list);
+    }
+}
