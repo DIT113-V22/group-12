a4976ecda3ba0bf18a8a0ea2aa59939f52387241
Kanokwan <gushaeka@student.gu.se>
2022-04-11 22:44:59 +0200

    Add car manual control via serial input

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -17,6 +17,9 @@ SimpleCar car(control);
 
 const auto forwardSpeed = 30;
 const auto stopSpeed = 0;
+const int bSpeed   = -70;
+const int lDegrees = -75;
+const int rDegrees = 75;
 
 void stopCar(){
     car.setSpeed(stopSpeed);
@@ -25,10 +28,42 @@ void startCar(){
     car.setSpeed(forwardSpeed);
 }
 
+
+void handleInput()
+{ // handle serial input if there is any
+    if (Serial.available())
+    {
+        char input = Serial.read(); // read everything that has been received so far and log down
+                                    // the last entry
+        switch (input)
+        {
+        case 'a': // rotate counter-clockwise going forward
+            car.setSpeed(forwardSpeed);
+            car.setAngle(lDegrees);
+            break;
+        case 'd': // turn clock-wise
+            car.setSpeed(forwardSpeed);
+            car.setAngle(rDegrees);
+            break;
+        case 'w': // go ahead
+            car.setSpeed(forwardSpeed);
+            car.setAngle(0);
+            break;
+        case 's': // go back
+            car.setSpeed(bSpeed);
+            car.setAngle(0);
+            break;
+        default: // if you receive something that you don't know, just stop
+            car.setSpeed(0);
+            car.setAngle(0);
+        }
+    }
+}
+
 void setup() {
 
     Serial.begin(9600);
-    startCar();
+    
 }
 
 void loop() {
@@ -38,7 +73,7 @@ void loop() {
     
     if(distance < 50 && distance > 0){
         stopCar();
-    }
+    } 
     
     delay(1);
 
