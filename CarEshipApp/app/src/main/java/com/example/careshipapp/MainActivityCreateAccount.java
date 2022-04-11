96b1bbffd9b3d444858aa9265683a266991146ef
d0nate110 <anton.golubenko@gmail.com>
2022-04-11 22:28:38 +0200

    Working on database

--- CarEshipApp/app/src/main/java/com/example/careshipapp/MainActivityCreateAccount.java
+++ CarEshipApp/app/src/main/java/com/example/careshipapp/MainActivityCreateAccount.java
@@ -3,12 +3,31 @@ package com.example.careshipapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivityCreateAccount extends AppCompatActivity {
+
+    EditText username, createPassword, passwordReentry;
+    MaterialButton createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_create_account);
+
+        username = (EditText) findViewById(R.id.Username);
+        createPassword = (EditText) findViewById(R.id.CreatePassword);
+        passwordReentry = (EditText) findViewById(R.id.passwordReentry);
+        createAccountButton = (MaterialButton) findViewById(R.id.CreateAccountButton);
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
