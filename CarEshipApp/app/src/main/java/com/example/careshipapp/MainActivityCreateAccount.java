717071dc49979756b9f37c7b50a3462c069a105a
d0nate110 <anton.golubenko@gmail.com>
2022-04-12 16:29:41 +0200

    Login and create account screen database implemented.

--- CarEshipApp/app/src/main/java/com/example/careshipapp/MainActivityCreateAccount.java
+++ CarEshipApp/app/src/main/java/com/example/careshipapp/MainActivityCreateAccount.java
@@ -2,9 +2,11 @@ package com.example.careshipapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -12,6 +14,7 @@ public class MainActivityCreateAccount extends AppCompatActivity {
 
     EditText username, createPassword, passwordReentry;
     MaterialButton createAccountButton;
+    DBHelperClass database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +25,59 @@ public class MainActivityCreateAccount extends AppCompatActivity {
         createPassword = (EditText) findViewById(R.id.CreatePassword);
         passwordReentry = (EditText) findViewById(R.id.passwordReentry);
         createAccountButton = (MaterialButton) findViewById(R.id.CreateAccountButton);
+        database = new DBHelperClass(this);
 
         createAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String email = username.getText().toString();
+                String passwrd = createPassword.getText().toString();
+                String repeatPassword = passwordReentry.getText().toString();
 
+                if(email.isEmpty() || passwrd.isEmpty() || repeatPassword.isEmpty()){
+                    Toast.makeText(MainActivityCreateAccount.this, "Please enter non-empty values.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if(passwrd.equals(repeatPassword)){
+                        Boolean customerChecking = database.userExistsCheck(email, passwrd);
+                        Boolean adminChecking = database.adminExistsCheck(email);
+
+                        if(!customerChecking){
+                            Boolean checkInsertedData = database.insertData(email, passwrd);
+                            if(checkInsertedData){
+                                Toast.makeText(MainActivityCreateAccount.this, "Account created successfully.", Toast.LENGTH_SHORT).show();
+                               /* Intent intent = new Intent(getApplicationContext(), nextscreen); this action leads to the next screen after creation of the account.
+                                startActivity(intent);*/
+                            }
+                            else{
+                                Toast.makeText(MainActivityCreateAccount.this, "Failed to create the account", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                        else if(!adminChecking){
+                            Boolean checkInsertedData = database.insertData(email, passwrd);
+                            if(checkInsertedData){
+                                Toast.makeText(MainActivityCreateAccount.this, "Account created successfully.", Toast.LENGTH_SHORT).show();
+                               /* Intent intent = new Intent(getApplicationContext(), nextscreen); this action leads to the next screen after creation of the account.
+                                startActivity(intent);*/
+                            }
+                            else{
+                                Toast.makeText(MainActivityCreateAccount.this, "Failed to create the account.", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                        else{
+                            Toast.makeText(MainActivityCreateAccount.this, "Account already exists.", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                    else{
+                        Toast.makeText(MainActivityCreateAccount.this, "Please repeat the password.", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }
             }
         });
     }
