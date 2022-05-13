156c04be91b0469953373726d95d22a2e7f7a0a3
Kanokwan <gushaeka@student.gu.se>
2022-05-14 01:14:13 +0200

    Add Introduction slider
    Adding introduction pages about our application for better understanding to our users.

---
+++ CarEshipApp/app/src/main/java/com/example/careshipapp/user_management/OnBoardingActivity.java
@@ -0,0 +1,100 @@
+package com.example.careshipapp.user_management;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import com.example.careshipapp.R;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+    SliderAdapter sliderAdapter;
+    TextView[] dots;
+    Button btn;
+    Animation animation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //hide status bar
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setContentView(R.layout.activity_on_boarding);
+
+        //hide toolbar
+        //getSupportActionBar().hide();
+
+        viewPager = findViewById(R.id.slider);
+        dotsLayout = findViewById(R.id.dots);
+        btn = findViewById(R.id.get_started_btn);
+        addDots(0);
+        viewPager.addOnPageChangeListener(changeListener);
+
+        // call adapter
+        sliderAdapter = new SliderAdapter(this);
+        viewPager.setAdapter(sliderAdapter);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OnBoardingActivity.this,UserActivityController.class));
+                finish();
+            }
+        });
+
+    }
+
+    private void addDots(int position){
+        dots = new TextView[3];
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dotsLayout.addView(dots[i]);
+        }
+
+
+        if (dots.length > 0) {
+            dots[position].setTextColor(getResources().getColor(R.color.pink));
+        }
+
+    }
+
+    ViewPager.OnPageChangeListener changeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+            addDots(position);
+
+            if(position == 0){
+                btn.setVisibility(View.INVISIBLE);
+            }else if (position == 1){
+                btn.setVisibility(View.INVISIBLE);
+            }else{
+                btn.setVisibility(View.VISIBLE);
+            }
+
+            }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+}
