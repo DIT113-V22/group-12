f6534f878e82e102bed5f09893e58aacb9d5fb8f
Nazli <101867745+nazlimoghaddam@users.noreply.github.com>
2022-05-18 14:20:20 +0200

    Adding the search filter
    This search filter optimises user experience while going through products

--- CarEshipApp/app/src/main/java/com/example/careshipapp/gui/adapters/ShowAllAdapter.java
+++ CarEshipApp/app/src/main/java/com/example/careshipapp/gui/adapters/ShowAllAdapter.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,6 +20,7 @@ import com.example.careshipapp.gui.models.ShowAllModel;
 import com.example.careshipapp.R;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShowAllAdapter extends RecyclerView.Adapter<ShowAllAdapter.ViewHolder> {
@@ -25,28 +28,30 @@ public class ShowAllAdapter extends RecyclerView.Adapter<ShowAllAdapter.ViewHold
     private Context context;
     private List<ShowAllModel> list;
 
+
     public ShowAllAdapter(Context context, List<ShowAllModel> list) {
         this.context = context;
         this.list = list;
+
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.show_all_item,parent,false));
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.show_all_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Glide.with(context).load(list.get(position).getImg_url()).into(holder.mItemImage);
-        holder.mCost.setText( "$ : " + (list.get(position).getPrice()));
+        holder.mCost.setText("$ : " + (list.get(position).getPrice()));
         holder.mName.setText(list.get(position).getName());
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(context, DetailedActivity.class);
-                intent.putExtra("detailed",list.get(holder.getAdapterPosition()));
+                intent.putExtra("detailed", list.get(holder.getAdapterPosition()));
                 context.startActivity(intent);
             }
         });
@@ -56,6 +61,11 @@ public class ShowAllAdapter extends RecyclerView.Adapter<ShowAllAdapter.ViewHold
     @Override
     public int getItemCount() {
         return list.size();
+    }
+
+    public void filterList(ArrayList<ShowAllModel> filteredList) {
+        list = filteredList;
+        notifyDataSetChanged();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -70,5 +80,10 @@ public class ShowAllAdapter extends RecyclerView.Adapter<ShowAllAdapter.ViewHold
             mCost = itemView.findViewById(R.id.item_cost);
             mName = itemView.findViewById(R.id.item_nam);
         }
-    }
-}
+
+
+            }
+
+        };
+
+
