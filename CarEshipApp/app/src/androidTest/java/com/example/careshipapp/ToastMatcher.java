e2a1f5bc316213a1dde4660706dc0a0d51e9499c
Akuien <91293028+Akuien@users.noreply.github.com>
2022-05-18 23:48:33 +0200

    Android app instrumental testing for the UI: the DrawableMatcher, RecyclerViewMatcher and ToastMatcher.

---
+++ CarEshipApp/app/src/androidTest/java/com/example/careshipapp/ToastMatcher.java
@@ -0,0 +1,34 @@
+package com.example.careshipapp;
+
+import android.os.IBinder;
+import android.view.WindowManager;
+
+import androidx.test.espresso.Root;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+/**
+ * Referenced by http://www.qaautomated.com/2016/01/how-to-test-toast-message-using-espresso.html
+ */
+
+public class ToastMatcher extends TypeSafeMatcher<Root> {
+    @Override
+    protected boolean matchesSafely(Root root) {
+        int type = root.getWindowLayoutParams().get().type;
+        if ((type == WindowManager.LayoutParams.TYPE_TOAST)) {
+            IBinder windowToken = root.getDecorView().getWindowToken();
+            IBinder appToken = root.getDecorView().getApplicationWindowToken();
+            if (windowToken == appToken) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("is toast");
+
+    }
+}
