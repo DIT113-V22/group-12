bdff48e6081de5686d2ecb2a9b1d373e43c12ea7
Marwa Selwaye <75786866+Marwasel@users.noreply.github.com>
2022-05-19 13:44:52 +0200

    Updated_payment

--- CarEshipApp/app/src/main/java/com/example/careshipapp/gui/activities/PaymentConfirmationActivity.java
+++ CarEshipApp/app/src/main/java/com/example/careshipapp/gui/activities/PaymentConfirmationActivity.java
@@ -6,40 +6,52 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.example.careshipapp.R;
-
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
-public class PaymentConfirmationActivity extends AppCompatActivity {
-    private TextView dateText;
-    private Calendar calendar;
-    private SimpleDateFormat dateFormat;
-    private String date;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.order_confirmation);
+    public class PaymentConfirmationActivity extends AppCompatActivity {
+        private TextView dateText, estimatedDelivery;
 
-        dateText = (TextView) findViewById(R.id.dateText);
+        private Calendar calendar;
+        private SimpleDateFormat dateFormat;
+        private String orderDate,estDate;
+        private Button backToMainBtn;
 
-        calendar = Calendar.getInstance();
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.order_confirmation);
 
-        dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-        date = dateFormat.format(calendar.getTime());
-        dateText.setText(date);
+            calendar = Calendar.getInstance();
 
-        Button btnTrackOrder = (Button) findViewById(R.id.buttonTrackOrder);
+            dateText = (TextView) findViewById(R.id.dateText);
 
-        btnTrackOrder.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(PaymentConfirmationActivity.this, TrackOrderActivity.class));
-            }
-        });
+            dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.GERMANY);
+            orderDate = dateFormat.format(calendar.getTime());
+            dateText.setText(orderDate);
 
-    }
+
+
+            estimatedDelivery = (TextView) findViewById(R.id.estimatedDeliveryDate);
+
+            dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.GERMANY);
+            calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH)+2);
+            estDate = dateFormat.format(calendar.getTime());
+            estimatedDelivery.setText(estDate);
+
+
+            backToMainBtn = (Button) findViewById(R.id.backButton);
+            backToMainBtn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+        startActivity(new Intent(PaymentConfirmationActivity.this, MainActivity.class));
+
+                }
+            });
+
+        }
 
     }
